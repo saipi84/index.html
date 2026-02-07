2abf2d1e75413a030a8086cc4b1bb9b6974d9a25
HOCH <saipi84@gmail.com>
2026-02-07 22:30:12 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,14 @@
-
-// firebase-config.js
 const firebaseConfig = {
-    apiKey: "FIREBASE_CONFIG_OVDE",
-    authDomain: "FIREBASE_CONFIG_OVDE",
-    databaseURL: "FIREBASE_CONFIG_OVDE",
-    projectId: "FIREBASE_CONFIG_OVDE",
-    storageBucket: "FIREBASE_CONFIG_OVDE",
-    messagingSenderId: "FIREBASE_CONFIG_OVDE",
-    appId: "FIREBASE_CONFIG_OVDE"
+  apiKey: "AIzaSyBXToxoncJIm7gRHYem4584EpoW_6bfwRU",
+  authDomain: "zsva-plan.firebaseapp.com",
+  databaseURL: "https://zsva-plan-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "zsva-plan",
+  storageBucket: "zsva-plan.firebasestorage.app",
+  messagingSenderId: "426759116582",
+  appId: "1:426759116582:web:9ac7dfdaa434b05cef53e7",
+  measurementId: "G-YGFT1B9M3R"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
