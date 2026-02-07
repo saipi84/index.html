7b2868df30b2511153fc1b960715229a020807e0
HOCH <saipi84@gmail.com>
2026-02-07 22:16:06 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,11 @@
 
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "FIREBASE_CONFIG_OVDE",
+    authDomain: "FIREBASE_CONFIG_OVDE",
+    databaseURL: "FIREBASE_CONFIG_OVDE",
+    projectId: "FIREBASE_CONFIG_OVDE",
+    storageBucket: "FIREBASE_CONFIG_OVDE",
+    messagingSenderId: "FIREBASE_CONFIG_OVDE",
+    appId: "FIREBASE_CONFIG_OVDE"
+};
